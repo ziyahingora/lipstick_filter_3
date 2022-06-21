e69e288d22eeda5fa1af49e12afc6de0bb4953b1
ziyahingora <93203286+ziyahingora@users.noreply.github.com>
2022-06-21 16:55:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+
+noseX=0;
+noseY=0;
+
+function preload() {
+    lipstick_nose = loadImage('https://i.postimg.cc/cHpZvvf7/lipstick-filter-removebg-preview.png')
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized')
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(lipstick_nose, noseX-15, noseY+15, 30, 30);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
